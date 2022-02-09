348ddc601e4fd5bedf61051149a923a3f6efb9b6
Chelsie L Bangloy-Gonsalves <clbgonsalves@gmail.com>
2022-02-08 19:42:06 -1000

    updates

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -42,16 +42,9 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => { //create new product
-    /*{ //req.body should look like this...
-        product_name: "Basketball",
-        price: 200.00,
-        stock: 3,
-        tagIds: [1, 2, 3, 4]
-      }*/
+router.post('/', (req, res) => {
     Product.create(req.body)
         .then((product) => {
-            // if there's product tags, we need to create pairings to bulk create in the ProductTag model
             if (req.body.tagIds.length) {
                 const productTagIdArr = req.body.tagIds.map((tag_id) => {
                     return {
@@ -71,27 +64,24 @@ router.post('/', (req, res) => { //create new product
         });
 });
 
-// update product
 router.put('/:id', (req, res) => {
-    // update product data
     Product.update(req.body, {
             where: { id: req.params.id, },
-        }).then((product) => { //find all associated tags from ProductTag
+        }).then((product) => { 
             return ProductTag.findAll({ where: { product_id: req.params.id } });
-        }).then((productTags) => { //get list of current tag_ids
+        }).then((productTags) => { 
             const productTagIds = productTags.map(({ tag_id }) => tag_id);
-            // create filtered list of new tag_ids
+
             const newProductTags = req.body.tagIds
                 .filter((tag_id) => !productTagIds.includes(tag_id))
                 .map((tag_id) => {
                     return { product_id: req.params.id, tag_id, };
                 });
 
-            // figure out which ones to remove
             const productTagsToRemove = productTags
                 .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
                 .map(({ id }) => id);
-            return Promise.all([ //run both actions
+            return Promise.all([
                 ProductTag.destroy({ where: { id: productTagsToRemove } }),
                 ProductTag.bulkCreate(newProductTags),
             ]);

f4b3418aedeb52e70557cbea1df05af7bdb26791
Chelsie Bangloy-Gonsalves <clbgonsalves@gmail.com>
2022-02-15 22:31:00 -1000

    will continue to work on server:

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
+
 router.get('/', (req, res) => {
   Category.findAll({
     attributes: ['id','category_name','product_name', 'price','stock','tag_name'],
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
+const express = require('express');
 const apiRoutes = require('./api');
 
 
 router.use('apiRoutes', apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>BITCH YOU DID</h1>")
+  res.send("Success!");
 });
 
 module.exports = router;
